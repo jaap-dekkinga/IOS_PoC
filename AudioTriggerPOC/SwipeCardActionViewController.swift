58db2f0bf3984dd89c476d0b453e5c1a978a9a87
Aleksandar Mihailovski <a.mihailovski@gmail.com>
2018-06-14 06:14:49 +0200

    Swipe screen UI changes
    Customize the swipe screen UI colors and copy.

--- AudioTriggerPOC/SwipeCardActionViewController.swift
+++ AudioTriggerPOC/SwipeCardActionViewController.swift
@@ -16,6 +16,15 @@ class SwipeCardActionViewController: UIViewController {
     fileprivate var sampleUrl: URL?
     fileprivate let voteManager = VoteManager()
 
+    fileprivate let greenYes = UIColor(red: 35.0/255.0,
+                                       green: 188.0/255.0,
+                                       blue: 73.0/255.0,
+                                       alpha: 1.0)
+    fileprivate let redNo = UIColor(red: 240.0/255.0,
+                                    green: 83.0/255.0,
+                                    blue: 73.0/255.0,
+                                    alpha: 1.0)
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,19 +36,59 @@ class SwipeCardActionViewController: UIViewController {
          */
 
         let viewGenerator: (String, CGRect) -> (UIView) = { (element: String, frame: CGRect) -> (UIView) in
-            let container = UIView(frame: CGRect(x: 30, y: 20, width: frame.width - 60, height: frame.height - 40))
-//            let label = UILabel(frame: container.bounds)
-//            label.text = element
-//            label.textAlignment = .center
-//            label.backgroundColor = UIColor.white
-//            label.font = UIFont.systemFont(ofSize: 48, weight: UIFont.Weight.thin)
-//            label.clipsToBounds = true
-//            label.layer.cornerRadius = 16
-//            container.addSubview(label)
+            let container = UIView(frame: CGRect(x: 30, y: 20,
+                                                 width: frame.width - 60,
+                                                 height: frame.height - 40))
+            container.backgroundColor = UIColor(red: 0.11,
+                                                green: 0.48,
+                                                blue: 0.53,
+                                                alpha: 1.00)
+            container.layer.cornerRadius = 20
+            container.clipsToBounds = true
 
-            let imageView = UIImageView(frame: container.bounds)
-            imageView.image = #imageLiteral(resourceName: "swipeScreen")
-            container.addSubview(imageView)
+            let touchViewWidth: CGFloat = container.frame.width/2
+            let touchViewHeight: CGFloat = touchViewWidth
+
+            let leftView = UIView(frame: CGRect(x: -touchViewWidth/4,
+                                                y: container.frame.height/2 - touchViewHeight/2,
+                                                width: touchViewWidth,
+                                                height: touchViewHeight))
+            leftView.backgroundColor = self.greenYes
+            leftView.layer.cornerRadius = touchViewWidth/2
+            container.addSubview(leftView)
+            let leftLabelRect = CGRect(x: leftView.bounds.origin.x + touchViewWidth/4,
+                                       y: leftView.bounds.origin.y,
+                                       width: leftView.bounds.width * 3/4,
+                                       height: leftView.bounds.height)
+            addLabel(in: leftView, text: "YES >", with: leftLabelRect)
+
+
+            let rightView = UIView(frame: CGRect(x: container.frame.width - touchViewWidth + touchViewWidth/4,
+                                                y: container.frame.height/2 - touchViewHeight/2,
+                                                width: touchViewWidth,
+                                                height: touchViewHeight))
+            rightView.backgroundColor = self.redNo
+            rightView.layer.cornerRadius = touchViewWidth/2
+            container.addSubview(rightView)
+            let rightLabelRect = CGRect(x: rightView.bounds.origin.x,
+                                        y: rightView.bounds.origin.y,
+                                        width: rightView.bounds.width * 3/4,
+                                        height: rightView.bounds.height)
+            addLabel(in: rightView, text: "< NO", with: rightLabelRect)
+
+            let labelHeight = CGFloat(100)
+            let label = UILabel(frame: CGRect(x: container.bounds.origin.x,
+                                              y: container.bounds.height - labelHeight,
+                                              width: container.bounds.width,
+                                              height: labelHeight))
+            label.text = element
+            label.textAlignment = .center
+            label.textColor = UIColor.white.withAlphaComponent(0.7)
+            label.font = UIFont.systemFont(ofSize: 26, weight: UIFont.Weight.thin)
+            label.clipsToBounds = true
+            label.layer.cornerRadius = 16
+            label.adjustsFontSizeToFitWidth = true
+            container.addSubview(label)
 
             container.layer.shadowRadius = 4
             container.layer.shadowOpacity = 1.0
@@ -51,12 +100,24 @@ class SwipeCardActionViewController: UIViewController {
             return container
         }
 
+        func addLabel(in container: UIView, text: String, with rect: CGRect? = nil) {
+            let rect = rect ?? container.bounds
+            let label = UILabel(frame: rect)
+            label.text = text
+            label.textAlignment = .center
+            label.font = UIFont.systemFont(ofSize: 32, weight: UIFont.Weight.thin)
+            label.textColor = UIColor.white.withAlphaComponent(0.7)
+            label.clipsToBounds = true
+            label.adjustsFontSizeToFitWidth = true
+            container.addSubview(label)
+        }
+
         let overlayGenerator: (SwipeMode, CGRect) -> (UIView) = { (mode: SwipeMode, frame: CGRect) -> (UIView) in
             let label = UILabel()
             label.frame.size = CGSize(width: 100, height: 100)
             label.center = CGPoint(x: frame.width / 2, y: frame.height / 2)
             label.layer.cornerRadius = label.frame.width / 2
-            label.backgroundColor = mode == .left ? UIColor.red : UIColor.green
+            label.backgroundColor = mode == .left ? self.redNo : self.greenYes
             label.clipsToBounds = true
             label.text = mode == .left ? "👎" : "👍"
             label.font = UIFont.systemFont(ofSize: 24)
@@ -75,7 +136,7 @@ class SwipeCardActionViewController: UIViewController {
     }
 
     func addCard() {
-        self.swipeView.addCards(["The One"], onTop: true)
+        self.swipeView.addCards(["Swipe to make your choice"], onTop: true)
         self.count = self.count + 1
     }
 }
