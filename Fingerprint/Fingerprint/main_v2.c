a2ba498178e3f41000bf753a35604b34394d9c24
Jaap Dekkinga <jaap.dekkinga@gmail.com>
2026-05-01 12:50:22 -0400

    V2 of the fingerprint code

---
+++ Fingerprint/Fingerprint/main_v2.c
@@ -0,0 +1,41 @@
+//
+//  main.c
+//  Fingerprint
+//
+//  Created by Gerrit Goossen <developer@gerrit.email> on 6/16/21.
+//  Copyright (c) 2021 TuneURL Inc. All rights reserved.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include "Fingerprint.h"
+
+int main(int argc, const char* argv[])
+{
+	for (int index = 0; index < argc; index++) {
+		if ((strcmp("fingerprint", argv[index]) == 0) && ((index + 1) <= argc)) {
+			const char *filePath = argv[++index];
+
+			printf("Extracting fingerprint: '%s'\n", filePath);
+
+			// generate the fingerprint
+			Fingerprint *fingerprint = ExtractFingerprintFromRawFile(filePath, FORMAT_VERSION_V1);
+			if (fingerprint == NULL) {
+				printf("Error generating fingerprint.\n");
+				continue;
+			}
+
+			// dump the fingerprint
+			printf("Fingerprint: [ %d", fingerprint->data[0]);
+			for (int c = 1; c < fingerprint->dataSize; c++) {
+				printf(", %d", fingerprint->data[c]);
+			}
+			printf(" ]\n");
+
+			// cleanup
+			FingerprintFree(fingerprint);
+		}
+	}
+
+	return 0;
+}
